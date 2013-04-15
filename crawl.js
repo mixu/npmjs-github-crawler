9745cc2f6fd49365e17cec7ae878e639c266297a
Mikito Takada <mikito.takada@gmail.com>
2013-04-15 09:27:32 -0700

    Implement github crawler, add cached results as tar.gz

--- crawl.js
+++ crawl.js
@@ -1,26 +1,46 @@
 var fs = require('fs'),
     client = require('mixu_minimal').Client;
 
+var repos = fs.readFileSync('./list.json')
+              .toString()
+              .split('\n')
+              .map(function(line) {
+                var result;
+                try {
+                  result = JSON.parse(line);
+                } catch(e) {}
+                return result;
+              }).filter(function(item) {
+                return !!item;
+              });
+
+// console.log(repos.length, repos[repos.length - 1]);
 
 function normalizeName(name) {
   // replace / with -
   return name.replace(/\//g, '-');
 }
 
-var moduleName = remaining.shift();
-while(fs.existsSync('./cache/'+normalizeName(moduleName)+'.json')) {
-  moduleName = remaining.shift();
-}
-
-/*
-
+var ghRe  = /^(?:https?:\/\/|git(?::\/\/|@))github.com[:\/](.*?)(?:.git)?$/;
 
 function fetchNext() {
+  var mod = repos.shift();
+  if(!mod) return;
+  while(!mod.key ||
+        !mod.url ||
+        fs.existsSync('./cache/'+normalizeName(mod.key)+'.json') ||
+        !(parts = mod.url.match(ghRe))) {
+    mod = repos.shift();
+    if(!mod) return;
+  }
 
-  console.log('Fetch:', user.name);
+  var ghUrl = parts[1];
+
+  console.log('Fetch:', ghUrl);
+
 
   client
-    .get('https://api.github.com/users/'+user.name)
+    .get('https://api.github.com/repos/'+ghUrl)
     .data({
     client_id: '',
     client_secret: '',
@@ -32,7 +52,7 @@ function fetchNext() {
       }
       (client.parse(function(err, res) {
         if (err) throw err;
-        fs.writeFileSync('./cache/users/'+user.name+'/meta.json', JSON.stringify(res, null, 2));
+        fs.writeFileSync('./cache/'+normalizeName(mod.key)+'.json', JSON.stringify(res, null, 2));
         console.log(res);
 
         if(h['x-ratelimit-remaining'] > 0) {
@@ -42,4 +62,5 @@ function fetchNext() {
     });
 
 }
-*/
+
+fetchNext();
